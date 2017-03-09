b7787fa0844058aff783d2ffe586c1e77c49340c
Thuyen Ngo <vanthuyen@gmail.com>
2017-03-09 12:48:58 -0800

    first commit

--- src/main.py
+++ src/main.py
@@ -2,13 +2,13 @@ import argparse
 import os
 import shutil
 import time
+import logging
 
 import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim
-import logging
 from torch.utils.data import DataLoader
 
 from data import ImageList, PEDataLoader
